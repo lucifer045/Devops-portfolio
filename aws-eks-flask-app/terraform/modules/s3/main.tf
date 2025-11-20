af7cf0bad21041a3bc05d640b3f6d615c6c80a70
lucifer045 <princegourlariya@gmail.com>
2025-11-21 00:04:17 +0530

    Updated S3 module

--- aws-eks-flask-app/terraform/modules/s3/main.tf
+++ aws-eks-flask-app/terraform/modules/s3/main.tf
@@ -1,20 +1,32 @@
 resource "aws_s3_bucket" "bucky" {
   bucket = var.bucket_name
-   
-  acl = "private" 
 
-  versioning {
-    enabled = true
+  tags = {
+    Name = "${var.env}-bucket"
   }
+}
 
-  server_side_encryption_configuration {
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.bucky.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+    bucket = aws_s3_bucket.bucky.id
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
     }
-  }
-  tags = {
-    Name = "${var.env}-bucket"
-  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state_block" {
+    bucket = aws_s3_bucket.bucky.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
 }
